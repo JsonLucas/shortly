d12d8ec2d6d64b3614e0d70204d449fbcf88fec7
pedro imbrech <elprogramadorpedro@gmail.com>
2022-06-24 11:24:44 -0300

    rev

--- database/queries/retrieve/sessions.js
+++
@@ -1,9 +0,0 @@
-import dbConnection from "../../dbConnect.js";
-
-const getSession = async (tokenContent) => {
-    const sql = `SELECT * FROM sessions WHERE "sessionToken" LIKE '%${tokenContent}%'`;
-    const query = await dbConnection.query(sql);
-    return query;
-}
-
-export default getSession;
