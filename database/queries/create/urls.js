df874e93a49cb407356c90a4adeee116ef8551d6
JsonLucas <jasonlucas443@gmail.com>
2022-10-02 23:59:46 -0300

    rebuild

--- database/queries/create/urls.js
+++
@@ -1,9 +0,0 @@
-import dbConnection from "../../dbConnect.js";
-
-const setShortenedUrl = async (body) => {
-    const { userId, fullUrl, shortUrl } = body;
-    const sql = `INSERT INTO urls ("userId", "fullUrl", "shortUrl") VALUES ($1, $2, $3)`;
-    await dbConnection.query(sql, [userId, fullUrl, shortUrl]);
-}
-
-export default setShortenedUrl;
--- database/queries/delete/sessions.js
+++
@@ -1,9 +0,0 @@
-import dbConnection from "../../dbConnect.js";
-
-const deleteSession = async (userId) => {
-    const sql = `DELETE FROM sessions WHERE "userId"=$1`;
-    const unlink = await dbConnection.query(sql, [userId]);
-    return unlink;
-}
-
-export default deleteSession;
